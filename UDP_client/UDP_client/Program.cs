459c42e32e0e4f701e1d33d4ad1356b71244bfdb
Ao Li <liao@live.dk>
2016-03-29 09:23:00 +0200

    yo

---
+++ UDP_client/UDP_client/Program.cs
@@ -0,0 +1,51 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UDP_client
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			byte[] packetData;
+			ConsoleKeyInfo cki;
+
+			const string IPadress = "192.168.217.130";
+			const int port = 9000;
+			var ep = new IPEndPoint(IPAddress.Parse(IPadress), port);
+			var client = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+			Console.WriteLine("(Q)uit, (U)ptime, (L)oadavg");
+			do
+			{
+				packetData = Encoding.ASCII.GetBytes("0");
+				cki = Console.ReadKey();
+				if(cki.Key == ConsoleKey.U)
+					packetData = Encoding.ASCII.GetBytes("U");
+				//proc/uptime
+				if (cki.Key == ConsoleKey.L)
+					packetData = Encoding.ASCII.GetBytes("L");
+				//proc/loadavg
+
+				client.SendTo(packetData, ep);
+
+
+			} while (cki.Key != ConsoleKey.Q && cki.Key != ConsoleKey.Escape);
+
+
+
+
+
+
+
+		}
+
+	}
+
+
+}
---
+++ UDP_server/UDP_server/Program.cs
@@ -0,0 +1,93 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Runtime.Remoting.Channels;
+
+namespace UDP_server
+{
+	class Runnshell
+	{
+		public Runnshell(string arg)
+		{
+			ProcessStartInfo psi = new ProcessStartInfo
+			{
+				FileName = "/tmp/bash.sh",
+				UseShellExecute = false,
+				RedirectStandardOutput = true,
+				Arguments = arg
+			};
+
+			Process p = Process.Start(psi);
+			string strOutput = p.StandardOutput.ReadToEnd();
+			p.WaitForExit();
+			Console.WriteLine(strOutput);
+		}
+	}
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var data = new byte[1000];
+			// lytter til alle IP adresser i port 9000
+			var endpoint = new IPEndPoint(IPAddress.Any,9000);
+			// storing connect from client 
+			var newSocket = new Socket(AddressFamily.InterNetwork,SocketType.Dgram,ProtocolType.Udp);
+			//bind inc connection to the socket
+			newSocket.Bind(endpoint);
+			Console.WriteLine("Waiting for client");
+
+			//waiting for connection  from any IP on port 9000
+			var sender = new IPEndPoint(IPAddress.Any, 9000);
+			//stores connection to tmpRemote
+			var tmpRemote = (EndPoint) sender;
+
+			// the stored data in data and size in recv
+			var recv = newSocket.ReceiveFrom(data, ref tmpRemote);
+
+			Console.WriteLine("message received from" + tmpRemote);
+			//convert the byte array into string
+			var datafromClient = Encoding.ASCII.GetString(data, 0, recv);
+			Console.WriteLine(datafromClient);
+			Runnshell rs;
+			switch (datafromClient)
+			{
+			case "U":
+				rs = new Runnshell("proc/uptime");
+				//use /proc/uptime
+				break;
+			case "L":
+				rs = new Runnshell("proc/loadavg");
+				// use /proc/loadavg
+				break;
+			default:
+				// dunno what to do
+				break;
+			}
+			string welcome = "welcome to my super server!, which is not copy/pasted";
+			data = Encoding.ASCII.GetBytes(welcome);
+
+			if (newSocket.Connected)
+				newSocket.Send(data);
+			while (true)
+			{
+				if (!newSocket.Connected)
+				{
+					Console.WriteLine("Client disconnected");
+					break;
+				}
+				data = new byte[1000];
+				recv = newSocket.ReceiveFrom(data, ref tmpRemote);
+				if (recv == 0)
+					break;
+				Console.WriteLine(Encoding.ASCII.GetString(data,0,recv));
+			}
+			newSocket.Close();
+		}
+	}
+}
