916b9cfb7e94f0da911a95a85ae56a7f31a8dbf1
Ao Li <201407737@post.au.dk>
2016-05-10 08:42:26 +0200

    lindasdsa

--- Exercise_13/Link/Link.cs
+++ Exercise_13/Link/Link.cs
@@ -103,12 +103,13 @@ namespace Linklaget
 		/// </param>
 		public int receive (ref byte[] buf)
 		{
-		    var x = serialPort.ReadBufferSize;
-		    var b = new byte[x];
-		    for (int i = 0; i < x; i++)
-		    {
-		    }
-		    var x1 = serialPort.Read(buffer, 0, x);
+		    //var x = serialPort.ReadBufferSize;
+		    //var b = new byte[x];
+		    //for (int i = 0; i < x; i++)
+		    //{
+		    //}
+
+		    var x1 = serialPort.Read(buffer, 0, buf.Length);
 
             Array.Copy(buf, 0, buffer, 1, buf.Length);
             // TO DO Your own code
